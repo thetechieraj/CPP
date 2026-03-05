b209dde84ab156585073127d3a5a4f306b8fdb01
thetechieraj <219003792+thetechieraj@users.noreply.github.com>
2026-03-05 23:29:45 +0530

    RSA2

--- Array/rotated_sorted_array.cpp
+++ Array/rotated_sorted_array.cpp
@@ -1,15 +1,29 @@
-class Solution {
-public:
-    bool check(vector<int>& nums) {
-        int n = nums.size();
-        int count = 0;
+#include<iostream>
+#include<vector>
 
-        for(int i = 0; i < n; i++) {
-            if(nums[i] > nums[(i + 1) % n]) {
-                count++;
-            }
+using namespace std;
+
+bool check(vector<int>& nums) {
+    int n = nums.size();
+    int count = 0;
+    
+    for(int i = 0; i < n; i++) {
+        if(nums[i] > nums[(i + 1) % n]) {
+            count++;
         }
-
-        return count <= 1;
     }
-};
+    return count <= 1;
+}
+
+int main(){
+    vector<int> nums = {4,5,1,2,3};
+
+    if(check(nums)){
+        cout << "Yes" << "\n";
+    }
+    else{
+        cout << "No" << "\n";
+    }
+
+    return 0;
+}
