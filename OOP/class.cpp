aee69cbbb4ffe4995c039439cc080f9ca11f2347
thetechieraj <219003792+thetechieraj@users.noreply.github.com>
2026-03-08 23:33:52 +0530

    OOP

---
+++ OOP/class.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class car{
+    // Data members (Attributes)
+    string name;
+    int manufacturingYear;
+    vector<string> model_Color_varient;
+
+    //Member Functions
+    
+};
+
+int main(){
+
+    return 0;
+}
