ddae8858f66c97c80115b73a6538f6942e7c91aa
thetechieraj <219003792+thetechieraj@users.noreply.github.com>
2026-04-06 23:47:22 +0530

    Anagram Update

--- Strings/anagram.cpp
+++ Strings/anagram.cpp
@@ -77,3 +77,27 @@ int main(){
 
 //     return 0;
 // }
+
+// class Solution {
+// public:
+//    bool isAnagram(string s, string t) {
+//         int freq1[256] = {0};
+
+//         for(int i = 0; i < s.length(); i++){
+//             freq1[(int)s[i]]++;
+//         }
+
+//         int freq2[256] = {0};
+
+//         for(int i = 0; i < t.length(); i++){
+//             freq2[(int)t[i]]++;
+//         }
+
+//         for(int i = 0; i < 256; i++){
+//             if(freq1[i] != freq2[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
