2653732172f86362c2db7cd947b052e138efd46e
thetechieraj <219003792+thetechieraj@users.noreply.github.com>
2026-07-16 23:31:32 +0530

    Add butterfyPattern

---
+++ Patterns/butterflyPattern.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }     
+        for(int j = i+1; j <= n; j++){
+            cout << " ";
+        }
+        for(int j = 1; j <= n-i; j++){
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    for(int i = n; i >= 1; i--){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        for(int j = i+1; j <= n; j++){
+            cout << " ";
+        }
+        for(int j = 1; j <= n-i; j++){
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
