93a402d470cf86113297d2de95e45c9a952fbbc7
thetechieraj <219003792+thetechieraj@users.noreply.github.com>
2026-08-14 22:29:09 +0530

    Add BFS

---
+++ Trees/bfs_implementation.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void BFS(int start, vector<vector<int>>& adj, int n) {
+    vector<bool> visited(n, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 6;
+
+    vector<vector<int>> adj(n);
+
+    adj[0] = {1, 2};
+    adj[1] = {0, 3, 4};
+    adj[2] = {0, 5};
+    adj[3] = {1};
+    adj[4] = {1};
+    adj[5] = {2};
+
+    BFS(0, adj, n);
+
+    return 0;
+}
