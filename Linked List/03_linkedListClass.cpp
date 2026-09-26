6b22aab81189dd3535d2a588674cec3dbcf208b2
thetechieraj <219003792+thetechieraj@users.noreply.github.com>
2026-09-26 23:16:36 +0530

    Add linkedListClass

--- Linked List/03_linkedListClass.cpp
+++ Linked List/03_linkedListClass.cpp
@@ -13,11 +13,13 @@ public:
 };
 
 class LinkedList{
-public:
+private:
     Node* head;
     Node* tail;
     int length;
-    
+
+public:
+
     LinkedList(){
         head = tail = NULL;
         length = 0;
@@ -47,6 +49,49 @@ public:
 
         length++;
     }
+
+    void insertAtHead(int num){
+        Node* n = new Node(num);
+
+        if(length == 0){
+            head = tail = n;
+        }
+        else{
+            n->next = head;
+            head = n;
+        }
+
+        length++;
+    }
+
+    void removeAtHead(){
+        if(length == 0){
+            cout << "List is Empty!" << "\n";
+            return;
+        }
+
+        head = head->next;
+        length--;
+    }
+
+    int size(){
+        return length;
+    }
+
+    void insert(int index, int val){
+        Node* n = new Node(val);
+
+        Node* temp = head;
+
+        for(int i = 0; i < index-1; i++){
+            temp = temp->next;
+        }
+
+        n->next = temp->next;
+        temp->next = n;
+
+        length++;
+    }
 };
 
 int main(){
@@ -55,6 +100,22 @@ int main(){
     one.insertAtTail(8);
     one.insertAtTail(5);
     one.insertAtTail(7);
+
+    // one.print();
+
+    // one.insertAtHead(4);
+    // one.insertAtHead(1);
+
+    // one.print();
+
+    // one.removeAtHead();
+
+    // one.print();
+
+    // cout << one.size();
+
+    one.insert(2,9);
+    one.insert(1,10);
 
     one.print();
 
